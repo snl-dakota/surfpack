2c9b1750ce44e456e741eccbd805be69f9aabeee
mricha <mricha@5a19d2f1-2ada-482b-9066-51d89674df4a>
2004-08-04 15:07:34 +0000

    Matlab function diffplot.m plots the differences between two surfaces.
    git-svn-id: https://software.sandia.gov/svn/surfpack/trunk@148 5a19d2f1-2ada-482b-9066-51d89674df4a

--- examples/diffplot.m
+++ examples/diffplot.m
@@ -1,11 +1,11 @@
-function H = snapshot(filename, column);
+function H = snapshot(filename, column1, column2);
 A = textread(filename);
 A = A(4:end,:);
 X = A(:,1);
 Y = A(:,2);
 V = A(:,3);
 W = A(:,4);
-Z = A(:,column);
+Z = A(:,column1) - A(:,column2);
 numpts = size(Z);
 ptsPerSide = sqrt(numpts(1));
 x1 = linspace(min(X),max(X),ptsPerSide);
