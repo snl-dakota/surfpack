860293ef9ac53a1747ac28f882a2ac98092c6da5
mricha <mricha@5a19d2f1-2ada-482b-9066-51d89674df4a>
2004-08-18 15:34:52 +0000

    Doxygen comments, removal of magic numbers
    git-svn-id: https://software.sandia.gov/svn/surfpack/trunk@167 5a19d2f1-2ada-482b-9066-51d89674df4a

--- src/surfpack.cpp
+++ src/surfpack.cpp
@@ -16,11 +16,11 @@
 
 #include "surfpack.h"
 
-//______________________________________________________________________________
-// Functions for use by Surface methods
-//______________________________________________________________________________
-
 using namespace std;
+
+// ____________________________________________________________________________
+// I/O 
+// ____________________________________________________________________________
 
 /// Write the value of contents to the file specified by filename.  Throw an
 /// exception if the file cannot be opened.
@@ -32,6 +32,110 @@ void surfpack::writeFile(std::string filename, std::string contents)
   }
   outfile << contents << endl;
   outfile.close();
+}
+
+
+/// Write the parameter header, followed by the matrix mat (the dimensions of
+/// which are specified by parameters rows and columns) to the parameter os.
+/// If c_style is true, the memory layout is assumed to follow the C
+/// convention (if mat points to an m by n matrix, the first m values are
+/// interpreted as the first row).  Otherwise, the layout is assumed to 
+/// follow the Fortran convention (the first n values are interpreted as the 
+/// first column).
+void surfpack::writeMatrix(const string header, double* mat, unsigned rows, 
+  unsigned columns, ostream& os, bool c_style)
+{
+  if (header != "none" && header != "") {
+    os << header << endl;
+  }
+  for (unsigned r = 0; r < rows; r++) {
+    for (unsigned c = 0; c < columns; c++) {
+      if (c_style) {
+        os << setw(surfpack::field_width) << mat[c + r * columns];
+      } else {
+        os << setw(surfpack::field_width) << mat[r + c * rows];
+      }
+    }
+    os << endl;
+  }
+}
+
+/// Write the parameter header, followed by the matrix mat (the dimensions of
+/// which are specified by parameters rows and columns) to the parameter os.
+/// If c_style is true, the memory layout is assumed to follow the C
+/// convention (if mat points to an m by n matrix, the first m values are
+/// interpreted as the first row).  Otherwise, the layout is assumed to 
+/// follow the Fortran convention (the first n values are interpreted as the 
+/// first column).
+/// \todo Templatize the method.  Priority: low.
+void surfpack::writeMatrix(const string header, unsigned* mat, unsigned rows, 
+  unsigned columns, ostream& os, bool c_style)
+{
+  if (header != "none" && header != "") {
+    os << header << endl;
+  }
+  for (unsigned r = 0; r < rows; r++) {
+    for (unsigned c = 0; c < columns; c++) {
+      if (c_style) {
+        os << setw(surfpack::field_width) << mat[c + r * columns];
+      } else {
+        os << setw(surfpack::field_width) << mat[r + c * rows];
+      }
+    }
+    os << endl;
+  }
+}
+
+/// Write the contents of a matrix to a file specified by parameter filename.
+/// Open the file and call another version of writeMatrix.
+void surfpack::writeMatrix(const string filename, double* mat, unsigned rows, 
+  unsigned columns, bool c_style)
+{
+  ofstream outfile(filename.c_str(),ios::out);
+  if (!outfile) {
+    throw file_open_failure(filename);
+  }
+  writeMatrix("none",mat,rows,columns,outfile, c_style);
+  outfile.close();
+}
+
+/// Write the contents of a matrix to a file specified by parameter filename.
+/// Open the file and call another version of writeMatrix.
+void surfpack::writeMatrix(const string filename, unsigned* mat, unsigned rows, 
+  unsigned columns, bool c_style)
+{
+  ofstream outfile(filename.c_str(),ios::out);
+  if (!outfile) {
+    throw file_open_failure(filename);
+  }
+  writeMatrix("none",mat,rows,columns,outfile, c_style);
+  outfile.close();
+}
+
+/// Write the parameter header followed by the values in the vector
+/// \todo Use an output iterator instead.  Priority: very low.
+void surfpack::printVector(const std::string header, vector<double>& vec,
+  ostream& os)
+{
+  os << header << " size: " << vec.size() << endl;
+  for (unsigned i = 0; i < vec.size(); i++) {
+    os << i << " " << vec[i] << endl;
+  }
+}
+
+/// Return true if the file specified by parameter file name has the extension
+/// specified by parameter extension
+bool surfpack::hasExtension(const std::string& filename, const std::string extension)
+{
+  return (filename.find(extension) == filename.size() - extension.size());
+}
+
+/// Throw an exception if end-of-file has been reached 
+void surfpack::checkForEOF(istream& is)
+{
+  if (is.eof()) {
+    throw surfpack::io_exception("End of file reached unexpectedly.");
+  }
 }
 
 /// Open the file specified by filename and return the type of Surface that 
@@ -57,7 +161,7 @@ const string surfpack::surfaceName(const string filename)
   infile.close();
   return nameInFile;
 } 
-  
+
 /// Return the next item in the file as a string.  If the file is opened in
 /// binary mode, first read an integer that specifies the number of 
 /// characters in the string, then read the string. 
@@ -75,6 +179,37 @@ const string surfpack::readName(istream& is, bool binary)
     surfaceType[nameSize] = '\0';
     return string(surfaceType);
   }
+}
+
+// ____________________________________________________________________________
+// Vector helper methods 
+// ____________________________________________________________________________
+
+/// Return the arithmetic mean (average) of the values in vector vals
+double surfpack::mean(std::vector<double>& vals)
+{
+  double sum = 0;
+  for (unsigned i = 0; i < vals.size(); i++) {
+    sum += vals[i];
+  }
+  return static_cast<double>(sum) / vals.size();
+}
+
+/// Return the sample variance of the values in vals
+double surfpack::sample_var(std::vector<double>& vals)
+{
+  double sse = 0;
+  double avg = surfpack::mean(vals);
+  for (unsigned i = 0; i < vals.size(); i++) {
+    sse += (vals[i]-avg)*(vals[i]-avg);
+  }
+  return sse / (vals.size() - 1);
+}
+
+/// Return the sample standard deviation of the values in vals
+double surfpack::sample_sd(std::vector<double>& vals)
+{
+  return sqrt(surfpack::sample_var(vals));
 }
 
 /// Return the euclidean distance between pt1 and pt2.  Throw an exception if
@@ -110,126 +245,14 @@ void surfpack::vectorDifference(vector<double>& diff, const vector<double>& pt1,
   }
 }
 
-/// Write the parameter header followed by the values in the vector
-/// \todo Use an output iterator instead.  Priority: very low.
-void surfpack::printVector(const std::string header, vector<double>& vec)
-{
-  cout << header << " size: " << vec.size() << endl;
-  for (unsigned i = 0; i < vec.size(); i++) {
-    cout << i << " " << vec[i] << endl;
-  }
-}
+// ____________________________________________________________________________
+// Testing 
+// ____________________________________________________________________________
 
-/// Return the arithmetic mean (average) of the values in vector vals
-double surfpack::mean(std::vector<double>& vals)
-{
-  double sum = 0;
-  for (unsigned i = 0; i < vals.size(); i++) {
-    sum += vals[i];
-  }
-  return static_cast<double>(sum) / vals.size();
-}
-
-/// Return the sample variance of the values in vals
-double surfpack::sample_var(std::vector<double>& vals)
-{
-  double sse = 0;
-  double avg = surfpack::mean(vals);
-  for (unsigned i = 0; i < vals.size(); i++) {
-    sse += (vals[i]-avg)*(vals[i]-avg);
-  }
-  return sse / (vals.size() - 1);
-}
-
-/// Return the sample standard deviation of the values in vals
-double surfpack::sample_sd(std::vector<double>& vals)
-{
-  return sqrt(surfpack::sample_var(vals));
-}
-
-/// Make sure eof has not been reached unexpectedly
-void surfpack::checkForEOF(istream& is)
-{
-  if (is.eof()) {
-    throw surfpack::io_exception("End of file reached unexpectedly.");
-  }
-}
-
-/// Write the parameter header, followed by the matrix mat (the dimensions of
-/// which are specified by parameters rows and columns) to the parameter os.
-/// If c_style is true, the memory layout is assumed to follow the C
-/// convention (if mat points to an m by n matrix, the first m values are
-/// interpreted as the first row).  Otherwise, the layout is assumed to 
-/// follow the Fortran convention (the first n values are interpreted as the 
-/// first column).
-void surfpack::writeMatrix(const string header, double* mat, unsigned rows, 
-  unsigned columns, ostream& os, bool c_style)
-{
-  if (header != "none" && header != "") {
-    cout << header << endl;
-  }
-  for (unsigned r = 0; r < rows; r++) {
-    for (unsigned c = 0; c < columns; c++) {
-      if (c_style) {
-        os << setw(15) << mat[c + r * columns];
-      } else {
-        os << setw(15) << mat[r + c * rows];
-      }
-    }
-    os << endl;
-  }
-}
-
-/// Write the parameter header, followed by the matrix mat (the dimensions of
-/// which are specified by parameters rows and columns) to the parameter os.
-/// If c_style is true, the memory layout is assumed to follow the C
-/// convention (if mat points to an m by n matrix, the first m values are
-/// interpreted as the first row).  Otherwise, the layout is assumed to 
-/// follow the Fortran convention (the first n values are interpreted as the 
-/// first column).
-/// \todo Templatize the method.  Priority: low.
-void surfpack::writeMatrix(const string header, unsigned* mat, unsigned rows, 
-  unsigned columns, ostream& os, bool c_style)
-{
-  if (header != "none" && header != "") {
-    cout << header << endl;
-  }
-  for (unsigned r = 0; r < rows; r++) {
-    for (unsigned c = 0; c < columns; c++) {
-      if (c_style) {
-        os << setw(15) << mat[c + r * columns];
-      } else {
-        os << setw(15) << mat[r + c * rows];
-      }
-    }
-    os << endl;
-  }
-}
-
-void surfpack::writeMatrix(const string filename, double* mat, unsigned rows, 
-  unsigned columns, bool c_style)
-{
-  ofstream outfile(filename.c_str(),ios::out);
-  if (!outfile) {
-    cerr << "Could not open file (" << filename << ") in writeMatrix." << endl;
-    return;
-  }
-  writeMatrix("none",mat,rows,columns,outfile, c_style);
-  outfile.close();
-}
-
-void surfpack::writeMatrix(const string filename, unsigned* mat, unsigned rows, 
-  unsigned columns, bool c_style)
-{
-  ofstream outfile(filename.c_str(),ios::out);
-  if (!outfile) {
-    cerr << "Could not open file (" << filename << ") in writeMatrix." << endl;
-    return;
-  }
-  writeMatrix("none",mat,rows,columns,outfile, c_style);
-  outfile.close();
-}
-
+/// Return the value of the test function specified by parameter name at the 
+/// point specified by parameter pt
+/// \todo Change if-else construct to lookup in STL map of <name, function>
+/// pairs.
 double surfpack::testFunction(const string name, const vector<double>& pt)
 {
   if (name == "rosenbrock") {
@@ -253,51 +276,7 @@ double surfpack::testFunction(const string name, const vector<double>& pt)
   }
 }
 
-double surfpack::sphere(const vector<double>& pt) 
-{
-  double result = 0.0;
-  for (unsigned i = 0; i < pt.size(); i++) {
-    double x = pt[i];
-    result += x*x;
-  }
-  return result;
-}
-
-double surfpack::quasisine(const std::vector<double>& pt)
-{
-  double result = 0.0;
-  double c = 16.0/15.0;
-  double e = 1.0;
-  for (unsigned i = 0; i < pt.size(); i++) {
-    double x = pt[i];
-    result += sin(c*x-e) + sin(c*x-e)*sin(c*x-e) + .02*sin(40.0*(c*x-e));
-  }
-  return result;
-
-}
-
-double surfpack::sinewave(const std::vector<double>& pt)
-{
-  double result = 0.0;
-  for (unsigned i = 0; i < pt.size(); i++) {
-    double x = pt[i];
-    result += sin(x);
-  }
-  return result;
-
-}
-
-double surfpack::xplussinex(const std::vector<double>& pt)
-{
-  double result = 0.0;
-  for (unsigned i = 0; i < pt.size(); i++) {
-    double x = pt[i];
-    result += x + sin(x);
-  }
-  return result;
-
-}
-
+/// Non-trivial polynomial function
 double surfpack::moderatepoly(const std::vector<double>& pt)
 {
   double result = -3.0;
@@ -310,19 +289,26 @@ double surfpack::moderatepoly(const std::vector<double>& pt)
     }
   }
   return result;
-
 }
 
-double surfpack::simplepoly(const std::vector<double>& pt)
+/// Tony Giunta's test function: a little wave mixed with a big wave, plus 
+/// noise
+double surfpack::quasisine(const std::vector<double>& pt)
 {
-  double result = 3.0;
+  double result = 0.0;
+  double c = 16.0/15.0;
+  double e = 1.0;
   for (unsigned i = 0; i < pt.size(); i++) {
     double x = pt[i];
-    result += 2.0*x;
+    result += sin(c*x-e) + sin(c*x-e)*sin(c*x-e) + .02*sin(40.0*(c*x-e));
   }
   return result;
 }
 
+/// f(x) = sigma{i=1 to n}(x_i^2 - 10*cos(2*pi*x) + 10).  With side 
+/// constraints near zero (e.g. +/-10) along each dimension, the function 
+/// appears highly-multimodal.  With larger bounds, the bowl shape becomes
+/// more dominant and the waviness is reduced to noise.
 double surfpack::rastrigin(const vector<double>& pt) 
 {
   double result = 0.0;
@@ -333,6 +319,7 @@ double surfpack::rastrigin(const vector<double>& pt)
   return result;
 }
 
+/// A multi-dimensional extension of the classic Rosenbrock test function
 double surfpack::rosenbrock(const vector<double>& pt) 
 {
   double result = 0.0;
@@ -344,6 +331,40 @@ double surfpack::rosenbrock(const vector<double>& pt)
   return result;
 }
 
+/// f(x) = 3 + sigma{i=1 to n}(2*x_i)
+double surfpack::simplepoly(const std::vector<double>& pt)
+{
+  double result = 3.0;
+  for (unsigned i = 0; i < pt.size(); i++) {
+    double x = pt[i];
+    result += 2.0*x;
+  }
+  return result;
+}
+
+/// Sum of the sine function along each dimension
+double surfpack::sinewave(const std::vector<double>& pt)
+{
+  double result = 0.0;
+  for (unsigned i = 0; i < pt.size(); i++) {
+    double x = pt[i];
+    result += sin(x);
+  }
+  return result;
+}
+
+/// Sum of squares along each dimension
+double surfpack::sphere(const vector<double>& pt) 
+{
+  double result = 0.0;
+  for (unsigned i = 0; i < pt.size(); i++) {
+    double x = pt[i];
+    result += x*x;
+  }
+  return result;
+}
+
+/// f(x) = sigma{i=1 to i}(x_i)
 double surfpack::sumofall(const vector<double>& pt) 
 {
   double result = 0.0;
@@ -353,8 +374,13 @@ double surfpack::sumofall(const vector<double>& pt)
   return result;
 }
 
-bool surfpack::hasExtension(const std::string& filename, const std::string extension)
+/// f(x) = sigma{i=1 to n}(x_i + sin x_i)
+double surfpack::xplussinex(const std::vector<double>& pt)
 {
-  return (filename.find(extension) == filename.size() - extension.size());
+  double result = 0.0;
+  for (unsigned i = 0; i < pt.size(); i++) {
+    double x = pt[i];
+    result += x + sin(x);
+  }
+  return result;
 }
-
--- src/surfpack.h
+++ src/surfpack.h
@@ -19,14 +19,27 @@ class SurfData;
 namespace surfpack {
 
 
-// ____________________________________________________________________________
+// _____________________________________________________________________________
+// Constants 
+// _____________________________________________________________________________
+  // Length of the field for double-precision number stream output
+  const unsigned field_width = 26;
+
+  // Precision of output for double precision numbers
+  const unsigned output_precision = 17;
+
+// _____________________________________________________________________________
 // Nested Types 
-// ____________________________________________________________________________
+// _____________________________________________________________________________
+
+  /// For use in comparing the actual value of some function with the estimate
+  /// given by a Surface approximation
   struct ErrorStruct {
     double observed;
     double estimated;
   };
 
+  /// Thrown when an attempt to open a file for reading or writing fails
   class file_open_failure: public std::runtime_error
   {
   public:
@@ -34,6 +47,9 @@ namespace surfpack {
       : std::runtime_error("File " + filename + " could not be opened.") {}
   };
     
+  /// Thrown when end-of-file is reached unexpectedly, when an unrecognized or
+  /// unacceptable file extension is encountered, or when a file contains
+  /// unexpected or illegally formatted contents.
   class io_exception: public std::runtime_error
   {
   public:
@@ -43,43 +59,61 @@ namespace surfpack {
 // ____________________________________________________________________________
 // I/O 
 // ____________________________________________________________________________
-
-  /// Make sure eof has not been reached unexpectedly
-  void checkForEOF(std::istream& is);
   
+  /// Write the value of contents to the file specified by filename.  Throw an
+  /// exception if the file cannot be opened.
+  void writeFile(std::string filename, std::string contents);
+
+  /// Write the parameter header, followed by the matrix mat (the dimensions of
+  /// which are specified by parameters rows and columns) to the parameter os.
+  /// If c_style is true, the memory layout is assumed to follow the C
+  /// convention (if mat points to an m by n matrix, the first m values are
+  /// interpreted as the first row).  Otherwise, the layout is assumed to 
+  /// follow the Fortran convention (the first n values are interpreted as the 
+  /// first column).
   void writeMatrix(const std::string header, double* mat, unsigned rows, 
     unsigned columns, std::ostream& os, bool c_style = false);
+
+  /// Write the parameter header, followed by the matrix mat (the dimensions of
+  /// which are specified by parameters rows and columns) to the parameter os.
+  /// If c_style is true, the memory layout is assumed to follow the C
+  /// convention (if mat points to an m by n matrix, the first m values are
+  /// interpreted as the first row).  Otherwise, the layout is assumed to 
+  /// follow the Fortran convention (the first n values are interpreted as the 
+  /// first column).
   void writeMatrix(const std::string header, unsigned* mat, unsigned rows, 
     unsigned columns, std::ostream& os, bool c_style = false);
+
+  /// Write the contents of a matrix to a file specified by parameter filename.
+  /// Open the file and call another version of writeMatrix.
   void writeMatrix(const std::string filename, double* mat, unsigned rows, 
     unsigned columns, bool c_style = false);
+
+  /// Write the contents of a matrix to a file specified by parameter filename.
+  /// Open the file and call another version of writeMatrix.
   void writeMatrix(const std::string filename, unsigned* mat, unsigned rows, 
     unsigned columns, bool c_style = false);
+
+  /// Write the parameter header followed by the values in the vector
+  void printVector(const std::string header, std::vector<double>& vec, 
+    std::ostream& os = cout);
+
+  /// Return true if the file specified by parameter file name has the extension
+  /// specified by parameter extension
   bool hasExtension(const std::string& filename, const std::string extension);
 
-  void printVector(const std::string header, std::vector<double>& vec);
+  /// Throw an exception if end-of-file has been reached 
+  void checkForEOF(std::istream& is);
 
-  void writeFile(std::string filename, std::string contents);
+  /// Open the file specified by filename and return the type of Surface that 
+  /// is written there.  Throw an exception if the file cannot be opened, or if
+  /// the file extension is anything other than .txt or .srf. 
   const std::string surfaceName(const std::string filename);
+
+  /// Return the next item in the file as a string.  If the file is opened in
+  /// binary mode, first read an integer that specifies the number of 
+  /// characters in the string, then read the string. 
   const std::string readName(std::istream& is, bool binary);
-  const unsigned field_width = 26;
-  const unsigned output_precision = 17;
-
-
-// ____________________________________________________________________________
-// Testing 
-// ____________________________________________________________________________
-  // test functions
-  double testFunction(const std::string name, const std::vector<double>& pt);
-  double simplepoly(const std::vector<double>& pt);
-  double quasisine(const std::vector<double>& pt);
-  double sinewave(const std::vector<double>& pt);
-  double xplussinex(const std::vector<double>& pt);
-  double moderatepoly(const std::vector<double>& pt);
-  double rosenbrock(const std::vector<double>& pt);
-  double rastrigin(const std::vector<double>& pt);
-  double sphere(const std::vector<double>& pt);
-  double sumofall(const std::vector<double>& pt);
 
 // ____________________________________________________________________________
 // Vector helper methods 
@@ -94,11 +128,54 @@ namespace surfpack {
   /// Return the sample standard deviation of the values in vals
   double sample_sd(std::vector<double>& vals);
   
-  /// Return the euclidean distance between pt1 and pt2
+  /// Return the euclidean distance between pt1 and pt2.  Throw an exception if
+  /// the dimensionality of the two vectors does not match.
   double euclideanDistance(const std::vector<double>& pt1, 
     const std::vector<double>& pt2);
+
+  /// Store the vector difference between pt1 and pt2 in the paramter diff.
+  /// Throw an exception if the dimensionality of the points does not match.
   void vectorDifference(std::vector<double>& diff, 
     const std::vector<double>& pt1, const std::vector<double>& pt2);
-  
+
+// ____________________________________________________________________________
+// Testing 
+// ____________________________________________________________________________
+  /// Return the value of the test function specified by parameter name at the 
+  /// point specified by parameter pt
+  /// \todo Change if-else construct to lookup in STL map of <name, function>
+  /// pairs.
+  double testFunction(const std::string name, const std::vector<double>& pt);
+
+  /// Non-trivial polynomial function
+  double moderatepoly(const std::vector<double>& pt);
+
+  /// Tony Giunta's test function: a little wave mixed with a big wave, plus 
+  /// noise
+  double quasisine(const std::vector<double>& pt);
+
+  /// f(x) = sigma{i=1 to n}(x_i^2 - 10*cos(2*pi*x) + 10).  With side 
+  /// constraints near zero (e.g. +/-10) along each dimension, the function 
+  /// appears highly-multimodal.  With larger bounds, the bowl shape becomes
+  /// more dominant and the waviness is reduced to noise.
+  double rastrigin(const std::vector<double>& pt);
+
+  /// A multi-dimensional extension of the classic Rosenbrock test function
+  double rosenbrock(const std::vector<double>& pt);
+
+  /// f(x) = 3 + sigma{i=1 to n}(2*x_i)
+  double simplepoly(const std::vector<double>& pt);
+
+  /// Sum of the sine function along each dimension
+  double sinewave(const std::vector<double>& pt);
+
+  /// Sum of squares along each dimension
+  double sphere(const std::vector<double>& pt);
+
+  /// f(x) = sigma{i=1 to i}(x_i)
+  double sumofall(const std::vector<double>& pt);
+
+  /// f(x) = sigma{i=1 to n}(x_i + sin x_i)
+  double xplussinex(const std::vector<double>& pt);
 } // namespace surfpack
 #endif
