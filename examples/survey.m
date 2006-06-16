c1a29bdd03a58a53bdd452746c17abae4ad9291b
mricha <mricha@5a19d2f1-2ada-482b-9066-51d89674df4a>
2006-06-16 15:35:26 +0000

    Removing unnecessary Matlab scripts from repository
    git-svn-id: https://software.sandia.gov/svn/surfpack/trunk@315 5a19d2f1-2ada-482b-9066-51d89674df4a

--- examples/survey.m
+++
@@ -1,8 +0,0 @@
-snapshot('biggrid.txt',3); pause
-snapshot('poly1test.txt',4); pause
-snapshot('poly2test.txt',4); pause
-snapshot('poly3test.txt',4); pause
-snapshot('krigtest.txt',4); pause
-snapshot('marstest.txt',4); pause
-snapshot('rbftest.txt',4); pause
-
