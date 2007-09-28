a16997f5158404a247bc9363d5f18dce867a5577
mricha <mricha@5a19d2f1-2ada-482b-9066-51d89674df4a>
2007-09-28 02:08:41 +0000

    - Removed SurfpackVector help class, in favor of a two-arg operator() for retrieving individual elements.  Although it gives matrix modification a Fortran feel, it also resolves some const/non-const issues I was having with individual element updates and also trims away a fair amount of extra code. + Added support for initialization of a matrix from a string; useful in unit tests + Added support for dimensional queries under the implicit transpose operator that is needed when these matrices are used in some lapack/blas routines via the fortran bindings. + Put the implementations of the methods in the .h file and eliminated the unconventional .tpl file altogether.
    git-svn-id: https://software.sandia.gov/svn/surfpack/trunk@393 5a19d2f1-2ada-482b-9066-51d89674df4a

--- src/SurfpackMatrix.h
+++ src/SurfpackMatrix.h
@@ -14,58 +14,6 @@
 #endif
 #include "surfpack_system_headers.h"
 
-/// Helper class for SurfpackMatrix.  Allows for the double subscripting of
-/// a contiguous chunk of memory.
-template< typename T >
-class SurfpackVector
-{
-public:
-  /// First argument gives address of first element of the row.
-  /// Second argument gives number of elements in the row.
-  SurfpackVector(T* ptr_to_first, unsigned n_elements);
-
-  /// First argument gives address of first element of the row.
-  /// Second argument gives number of elements in the row.
-  /// Third and fourth arguments are necessary if object is used in conjunction
-  /// with a SurfpackMatrix that is configured for passing to Fortran APIs.
-  /// Row
-  SurfpackVector(T* ptr_to_first, unsigned n_elements,
-    unsigned n_rows, bool for_fortran = true);
-
-  /// Default constructor.  Data members default to 0/false 
-  SurfpackVector();
-
-  /// Copy constructor.  Performs deep copy.
-  SurfpackVector(const SurfpackVector< T >& other);
-
-  /// Assignment operator.  Performs deep copy.
-  SurfpackVector<T>& operator=(const SurfpackVector< T >& other);
-
-  /// Sets the address of the first element of this row
-  SurfpackVector<T>& operator()(T* ptr_to_first);
-
-  /// Returns list of row elements, separated by spaces, NOT newline-terminated
-  const std::string asString();
-
-  /// Returns the element of the row indexed by the argument
-  T& operator[](unsigned vector_index);
-private:
-  /// First element of the row
-  T* ptrToFirst;
-
-  /// Number of elements in the row
-  unsigned nElements;
-
-  /// Number of rows in the Matrix for which this vector is a row.
-  /// Only needed if Matrix is configured for use with Fortran APIs
-  unsigned nRows;
-
-  /// True if the Matrix this row is a part of is configured for use with
-  /// Fortran.  If so, the elements of the row are not contiguous, but are
-  /// spaced every nRows elements apart in the contiguous block that the
-  bool forFortran;
-};
-  
 /// A SurfpackMatrix uses an STL vector-- where all elements reside in a 
 /// contiguous block of memory-- to represent a two-dimensional matrix.
 /// Overloading of operator[], together with the use of helper class
@@ -87,6 +35,7 @@ class SurfpackMatrix
 public:
   /// Creates a n_rows x n_cols matrix
   SurfpackMatrix(unsigned n_rows, unsigned n_cols, bool for_fortran = true);
+  SurfpackMatrix(unsigned n_rows, unsigned n_cols, const std::string& vals, bool for_fortran = true);
   
   /// Defaults to a 1x1 matrix for use with Fortran
   SurfpackMatrix(bool for_fortran = true);
@@ -94,19 +43,25 @@ public:
   /// Copy constructor.  Makes deep copy.
   SurfpackMatrix(const SurfpackMatrix<T>& other);
 
-  /// Resize the matrix.
+  /// Resize the matrix. 
+  ///\todo Deprecate this.  Use resize instead.  Or make it work like Matlab's.
   void reshape(unsigned n_rows, unsigned n_cols);
 
+  /// Resize the matrix; retain previous values where possible
+  void resize(unsigned n_rows, unsigned n_cols);
+
   /// Elements separated by spaces; rows delimited by newlines.
-  const std::string asString();
+  const std::string asString() const;
 
   /// Returns the elements of a matrix as a list, in the order they appear in
   /// memory.
-  const std::string asArrayString();
+  const std::string asArrayString() const;
 
-  /// For selecting a matrix row.  Should only be used as the first in a pair
-  /// of subscripts-- never alone.
-  SurfpackVector<T>& operator[](unsigned row_index);
+  /// retrieve a single element from the matrix by value
+  const T& operator()(unsigned r, unsigned c) const;
+
+  /// retrieve a single element from the matrix by reference
+  T& operator()(unsigned r, unsigned c);
 
   /// Assignment operator.  Performs deep copy.
   SurfpackMatrix<T>& operator=(const SurfpackMatrix<T>& other);
@@ -115,13 +70,13 @@ public:
   SurfpackMatrix<T>& operator=(const std::vector< std::vector<T> >);
   
   /// Sum of the elements of the matrix.  Data type T should support operator+.
-  T sum();
+  T sum() const;
 
   /// Number of rows in the matrix.
-  unsigned getNRows();
+  unsigned getNRows(char transpose = 'N') const;
 
   /// Number of columns in the matrix.
-  unsigned getNCols();
+  unsigned getNCols(char transpose = 'N') const;
 private:
   /// True iff matrix can be safely passed to a Fortran API.
   bool forFortran;
@@ -136,10 +91,174 @@ private:
   std::vector< T > rawData;
 
   /// Row of matrix most recently references by operator[]
-  SurfpackVector< T > oneRow;
+  //mutable SurfpackVector< T > oneRow;
 };
 
-// Definitions must be included in same file as declarations
-#include "SurfpackMatrix.tpl"
+// Implementation of functions
+template< typename T >
+SurfpackMatrix< T >::SurfpackMatrix(unsigned n_rows, unsigned n_cols,
+  bool for_fortran)
+  : forFortran(for_fortran), nRows(n_rows), nCols(n_cols)
+{
+  rawData.resize(nRows*nCols);
+}
+
+template< typename T >
+SurfpackMatrix< T >::SurfpackMatrix(bool for_fortran)
+  : forFortran(for_fortran), nRows(1), nCols(1) 
+{
+  rawData.resize(1);
+}
+
+template< typename T >
+SurfpackMatrix< T >::SurfpackMatrix(unsigned n_rows, unsigned n_cols,
+  const std::string& vals, bool for_fortran)
+  : forFortran(for_fortran), nRows(n_rows), nCols(n_cols)
+{
+  rawData.resize(nRows*nCols);
+  std::istringstream is(vals);
+  for (unsigned i = 0; i < rawData.size(); i++) {
+    is >> rawData[i];
+  }
+}
+
+template< typename T >
+SurfpackMatrix< T>::SurfpackMatrix(const SurfpackMatrix<T>& other)
+  : forFortran(other.forFortran), nRows(other.nRows), nCols(other.nCols),
+  rawData(other.rawData)
+{
+
+}
+
+
+template< typename T >
+void SurfpackMatrix< T >::reshape(unsigned n_rows, unsigned n_cols)
+{
+  nRows = n_rows;
+  nCols = n_cols;
+  rawData.resize(n_rows*n_cols);
+}
+
+template< typename T >
+void SurfpackMatrix< T >::resize(unsigned n_rows, unsigned n_cols)
+{
+  unsigned old_rows = nRows;
+  unsigned old_cols = nCols;
+  nRows = n_rows;
+  nCols = n_cols;
+  std::vector< T > old_data = rawData;
+  // Now go back and make sure that all of the elements in the new
+  // matrix that were also present in the old matrix retain their values
+  //unsigned row_max = min(old_rows,nRows);
+  unsigned row_max = (old_rows < nRows) ? old_rows : nRows;
+  //unsigned col_max = min(old_cols,nCols);
+  unsigned col_max = (old_cols < nCols) ? old_cols : nCols;
+  rawData.resize(nRows*nCols);
+  for (unsigned i = 0; i < nRows; i++) {
+    for (unsigned j = 0; j < nCols; j++) {
+      unsigned dest_index = forFortran ? j*nRows+i : i*nCols+j;
+      if (i >= old_rows || j >= old_cols) {
+        rawData[dest_index] = 0;
+      } else {
+        unsigned src_index = forFortran ? j*old_rows+i : i*old_cols+j;
+        rawData[dest_index] = old_data[src_index];
+      } 
+    }
+  }
+}
+
+template< typename T >
+SurfpackMatrix< T >& SurfpackMatrix< T >::operator=(const SurfpackMatrix<T>& other)
+{
+ ///\todo check for assignment to self
+ forFortran = other.forFortran;
+ nRows = other.nRows;
+ nCols = other.nCols;
+ rawData = other.rawData;
+ return *this;
+}
+
+template< typename T >
+SurfpackMatrix<T>& SurfpackMatrix< T >::operator=(const std::vector< std::vector<T> > other)
+{
+  if (other.empty()) {
+    reshape(0,0);
+  } else {
+    reshape(other.size(),other[0].size());
+  } 
+  ///\todo Use assign instead of using this clunky double loop
+  for (unsigned i = 0; i < nRows; i++) {
+    for (unsigned j = 0; j < nCols; j++) {
+      (*this)(i,j) = other[i][j];
+    }
+  }
+  return *this;
+
+}
+
+template< typename T >
+const std::string SurfpackMatrix< T >::asString() const
+{
+  std::ostringstream os;
+  unsigned index;
+  for (unsigned i = 0; i < nRows; i++) {
+    // In C, members of the row are contiguous.  In Fortran, if the matrix has
+    // n rows, the elements of any row are spaced n elements apart.
+    for (unsigned r = 0; r < nRows; r++) {
+      for (unsigned c = 0; c < nCols; c++) {
+        os << (*this)(r,c) << " ";
+      }
+      os << " ";
+    }
+  }
+  return os.str();
+}
+
+template< typename T >
+const std::string SurfpackMatrix< T >::asArrayString() const
+{
+  std::ostringstream os;
+  for (unsigned i = 0; i < rawData.size(); i++) {
+    os << rawData[i] << " "; 
+  }
+  return os.str();
+}
+
+template< typename T >
+unsigned SurfpackMatrix< T >::getNRows(char transpose) const
+{
+  assert(transpose == 'N' || transpose == 'T');
+  return (transpose == 'N') ? nRows : nCols;
+}
+
+template< typename T >
+unsigned SurfpackMatrix< T >::getNCols(char transpose) const
+{
+  assert(transpose == 'N' || transpose == 'T');
+  return (transpose == 'N') ? nCols : nRows;
+}
+
+template< typename T >
+const T& SurfpackMatrix<T>::operator()(unsigned r, unsigned c) const
+{
+  return const_cast< SurfpackMatrix<T>& >(*this)(r,c);
+}
+
+template< typename T >
+T& SurfpackMatrix<T>::operator()(unsigned r, unsigned c)
+{
+  if (forFortran) {
+    return rawData[c*nRows+r];
+  }
+  return rawData[r*nCols+c];
+}
+
+template< typename T >
+T SurfpackMatrix<T>::sum() const
+{
+  return std::accumulate(rawData.begin(),rawData.end(),0.0);
+}
+
+typedef SurfpackMatrix<double> MtxDbl;
 
 #endif
