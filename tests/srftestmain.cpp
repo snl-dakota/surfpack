188df83c8fec0c843c56c9bab5f4ff86e1691016
mricha <mricha@5a19d2f1-2ada-482b-9066-51d89674df4a>
2007-11-02 18:29:10 +0000

    Adding new tests and updating Makefile.am and srftestmain.cpp to reflect changes
    git-svn-id: https://software.sandia.gov/svn/surfpack/trunk@407 5a19d2f1-2ada-482b-9066-51d89674df4a

--- tests/srftestmain.cpp
+++ tests/srftestmain.cpp
@@ -12,23 +12,64 @@
 
 //#include "StdAfx.h"
 #include <cppunit/CompilerOutputter.h>
+#include <cppunit/XmlOutputter.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 #include "unittests.h"
+#include "SurfaceTest.h"
+#include "SurfDataTest.h"
+#include "SurfScalerTest.h"
+//#include "PolynomialSurfaceTest.h"
+#include "SurfPointTest.h"
+#include "SurfpackCommonTest.h"
+#include "LinearRegressionModelTest.h"
+#include "MovingLeastSquaresTest.h"
+#include "RadialBasisFunctionTest.h"
+#include "SurfpackModelTest.h"
+#include "KrigingModelTest.h"
+#include "ModelScalerTest.h"
+#include "ModelFactoryTest.h"
 #include <string>
 
 int main(int argc, char* argv[])
 {
   // Get the top level suite from the registry
-  CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+  //CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+  CppUnit::Test *surfpoint_suite = SurfPointTest::suite(); 
+  CppUnit::Test *surf_data_suite = SurfDataTest::suite(); 
+  CppUnit::Test *surf_scaler_suite = SurfScalerTest::suite(); 
+  CppUnit::Test *surface_suite = SurfaceTest::suite(); 
+  //CppUnit::Test *polynomial_suite = PolynomialSurfaceTest::suite(); 
+  CppUnit::Test *surfpack_common_suite = SurfpackCommonTest::suite(); 
+  CppUnit::Test *linear_regression_model_suite = LinearRegressionModelTest::suite(); 
+  CppUnit::Test *surfpack_model_suite = SurfpackModelTest::suite(); 
+  CppUnit::Test *model_scaler_suite = ModelScalerTest::suite(); 
+  CppUnit::Test *moving_least_squares_suite = MovingLeastSquaresTest::suite(); 
+  CppUnit::Test *radial_basis_function_suite = RadialBasisFunctionTest::suite(); 
+  CppUnit::Test *kriging_model_test_suite = KrigingModelTest::suite(); 
+  CppUnit::Test *model_factory_test_suite = ModelFactoryTest::suite(); 
 
   // Adds the test to the list of test to run
   CppUnit::TextUi::TestRunner runner;
-  runner.addTest( suite );
+  //runner.addTest( surfpoint_suite );
+  //runner.addTest( surf_data_suite );
+  //runner.addTest( surf_scaler_suite );
+  //runner.addTest( surface_suite );
+  //runner.addTest( polynomial_suite );
+  //runner.addTest( surfpack_common_suite );
+  //runner.addTest( linear_regression_model_suite );
+  //runner.addTest( surfpack_model_suite );
+  //runner.addTest( moving_least_squares_suite );
+  //runner.addTest( radial_basis_function_suite );
+  //runner.addTest( model_scaler_suite );
+  //runner.addTest( kriging_model_test_suite );
+  runner.addTest( model_factory_test_suite );
 
   // Change the default outputter to a compiler error format outputter
   runner.setOutputter( new CppUnit::CompilerOutputter( &runner.result(),
                                                        std::cerr ) );
+  //runner.setOutputter( new CppUnit::XmlOutputter( &runner.result(),
+  //                                                     std::cerr ) );
   // Run the test.
   bool wasSucessful = runner.run();
 
