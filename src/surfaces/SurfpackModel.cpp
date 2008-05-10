e18ee4fabb6612d107bd8283aec986af4279185d
lpswile <lpswile@5a19d2f1-2ada-482b-9066-51d89674df4a>
2008-05-10 19:24:59 +0000

    This is the first commit to demonstrate how we will calculate and expose Surfpack diagnostics through the DAKOTA interface. Unfortunately, with the overhaul Mark made to the Surfpack classes and the switch from Surfaces to Model/Factory, these diagnostics all need to be re-implemented.  This commit only reimplements R^2. The idea is to reimplement all of the diagnostics in the SurfpackModel class. Brian - if you can take a quick look and see if this seems like the correct framework to use, that would be great. I did borrow heavily from the Surfaces implementation. At some point, we need to use this new framework to build a standalone Surfpack version.  To my knowledge, the Dakota library and standalone version are diverging.
    git-svn-id: https://software.sandia.gov/svn/surfpack/trunk@432 5a19d2f1-2ada-482b-9066-51d89674df4a

--- src/surfaces/SurfpackModel.cpp
+++ src/surfaces/SurfpackModel.cpp
@@ -64,6 +64,119 @@ MtxDbl SurfpackModel::hessian(const VecDbl& x) const
   throw std::string("This model does not currently support hessians");
 }
 
+double SurfpackModel::goodnessOfFit(const string metricName, const SurfData& surf_data) 
+{
+  // need to put error message if surfData not populated
+  std::cout << " Calculating goodness of fit " << "\n" ;
+  if (metricName == "rSquared") {
+    return rSquared(surf_data);
+  } else if (metricName == "press") {
+    return nFoldCrossValidation(surf_data,surf_data.size());
+  } else {
+    // The rest of these metrics all have many computations in common
+    // and are grouped together in the genericMetric method
+    vector<double> observed(surf_data.size());
+    vector<double> predicted(surf_data.size());
+    observed = surf_data.getResponses();
+    for (unsigned i = 0; i < surf_data.size(); i++) {
+     predicted[i]=(*this)(surf_data(i));
+    }
+    //predicted = surf_data.getPredictor();
+    //getValue(surfData, observed, predicted);
+    if (metricName == "min_abs" ) {
+      return genericMetric(observed,predicted,MT_MINIMUM,ABSOLUTE);
+    } else if (metricName == "max_abs") {
+      return genericMetric(observed,predicted,MT_MAXIMUM,ABSOLUTE);
+    } else if (metricName == "sum_abs") {
+      return genericMetric(observed,predicted,MT_SUM,ABSOLUTE);
+    } else if (metricName == "mean_abs") {
+      return genericMetric(observed,predicted,MT_MEAN,ABSOLUTE);
+    } else if (metricName == "max_relative") {
+      return genericMetric(observed,predicted,MT_RELATIVE_MAXIMUM,ABSOLUTE);
+    } else if (metricName == "mean_relative") {
+      return genericMetric(observed,predicted,MT_RELATIVE_AVERAGE,ABSOLUTE);
+    } else if (metricName == "min_squared" ) {
+      return genericMetric(observed,predicted,MT_MINIMUM,SQUARED);
+    } else if (metricName == "max_squared") {
+      return genericMetric(observed,predicted,MT_MAXIMUM,SQUARED);
+    } else if (metricName == "sum_squared") {
+      return genericMetric(observed,predicted,MT_SUM,SQUARED);
+    } else if (metricName == "mean_squared") {
+      return genericMetric(observed,predicted,MT_MEAN,SQUARED);
+    } else if (metricName == "min_scaled" ) {
+      return genericMetric(observed,predicted,MT_MINIMUM,SCALED);
+    } else if (metricName == "max_scaled") {
+      return genericMetric(observed,predicted,MT_MAXIMUM,SCALED);
+    } else if (metricName == "sum_scaled") {
+      return genericMetric(observed,predicted,MT_SUM,SCALED);
+    } else if (metricName == "mean_scaled") {
+      return genericMetric(observed,predicted,MT_MEAN,SCALED);
+    //} else if (metricName == "root_mean_squared") {
+    //  return rootMeanSquared(observed,predicted);
+    //} else if (metricName == "l2norm") {
+    //  return sqrt(genericMetric(observed,predicted,MT_SUM,SQUARED));
+    } else {
+      throw string("No error metric of that type in this class");
+    }
+  }
+}
+ 
+double SurfpackModel::rSquared(const SurfData& surf_data)
+{
+  // Sum of the function evaluations for all the points; used to compute mean
+  double sumObserved = 0.0;
+ 
+  // Sum of the squared function evaluations for the points; used to compute
+  // the total sum of squares
+  double sumOfSquaresObserved = 0.0;
+ 
+  // Sum of the squared differences between the true function value and the
+  // Surface approximation's estimate of the function value over all of the
+  // points
+  double residualSumOfSquares = 0.0;
+ 
+  // Sum of the squared differences between the true function value and mean
+  // function value over all of the points
+  double totalSumOfSquares = 0.0;
+ 
+  for (unsigned i = 0; i < surf_data.size(); i++) {
+    double observedF = surf_data.getResponse(i);
+    std::cout << "observedF " << observedF;
+    double estimatedF = (*this)(surf_data(i));
+    std::cout << "estimatedF " << estimatedF;
+    //double estimatedF = surf_data.getPredictor(i);
+    double residual = observedF - estimatedF;
+    std::cout << "residual " << residual << "\n";
+    residualSumOfSquares += residual * residual;
+    sumObserved += observedF;
+    sumOfSquaresObserved += observedF * observedF;
+  }
+  // This is the same as sigma{i=1 to n}(x_i - xbar)^2
+  totalSumOfSquares = sumOfSquaresObserved -
+    (sumObserved * sumObserved / surf_data.size());
+  double rSquaredValue = 1.0 - residualSumOfSquares / totalSumOfSquares;
+  // In a polynomial regression, this computation will always result in a value
+  // between 0 and 1, because the residual sum of squares will always be less
+  // than or equal to the total sum of squares (i.e., the worst your regression
+  // can do is give you the mean everywhere).  Some of the other methods (Mars,
+  // Kriging, RBF, etc.) can have much larger residual sum of squares.  We will
+  // refrain from returning a value less than zero however.
+  return (rSquaredValue < 0) ? 0 : rSquaredValue;
+}
+
+double SurfpackModel::press(const SurfData& data)
+{
+}
+
+double SurfpackModel::nFoldCrossValidation(const SurfData& data, unsigned n)
+{
+}
+
+double SurfpackModel::genericMetric(std::vector<double>& observed,
+    std::vector<double>& predicted, enum MetricType mt, enum DifferenceType dt)
+{
+}
+
 void SurfpackModel::scaler(ModelScaler* ms)
 {
   delete mScaler;
@@ -143,6 +256,8 @@ SurfpackModel* SurfpackModelFactory::Build(const SurfData& sd)
   sd.setDefaultIndex(this->response_index);
   if (sd.size() < minPointsRequired()) {
     throw string("Not enough Points");
+    std::cout << " size of data " << sd.size();
+    std::cout << " minPointsRequired " << minPointsRequired();
   }
   SurfpackModel* model = Create(sd);
   model->parameters(params);
--- src/surfaces/SurfpackModel.h
+++ src/surfaces/SurfpackModel.h
@@ -15,6 +15,7 @@
 #include "surfpack_system_headers.h"
 #include "ModelScaler.h"
 #include "SurfpackMatrix.h"
+#include "surfpack.h"
 
 class SurfData;
 
@@ -39,6 +40,35 @@ public:
   virtual VecDbl gradient(const VecDbl& x) const;
   virtual MtxDbl hessian(const VecDbl& x) const;
   virtual std::string asString() const = 0;
+  /// return the value of some error metric
+  double goodnessOfFit(const std::string MetricName, const SurfData& data); 
+  ///double goodnessOfFit(const std::string MetricName); 
+  /// Return R^2, which measures the proportion of variability in the data 
+  /// accounted for by the model (the approximating surface).
+  /// R^2 = 1-SSE/SST = SSR/SST. 
+  double rSquared(const SurfData& data);
+  /// For each point x in dataSet, construct a similar approximation Surface
+  /// that includes all of the points in dataSet except x.  Then evaluate the
+  /// Surface at x.  The difference between x and the estimate of x given the
+  /// rest of the data is the residual for x.  The PRESS statistic is the
+  /// square root of the mean of the squares of all the residuals.
+  double press(const SurfData& data);
+  double nFoldCrossValidation(const SurfData& data, unsigned n);
+  /// Compute one of several goodness of fit metrics.  The observed parameter
+  /// should be a list of observed (or true) function values; the vector of
+  /// predicted values gives the corresponding estimates from this surface.
+  /// The dt parameter specifies the kind of residuals to compute.  ABSOLUTE
+  /// residuals are (observed - predicted), SQUARED residuals are the squares
+  /// of the absolute residuals.  SCALED residuals are the ABSOLUTE residuals
+  /// divided by the observed value.  Given the type of residuals, the client
+  /// may request the min, max, sum, or mean of the set of residuals over all
+  /// the given data points.  Two additional metrics are possible.  The
+  /// relative maximum absolute error is the maximum absolute error divided
+  /// by the standard deviation of the observed data.  The relative average
+  /// absolute error is the mean absolute error divided by the standard
+  /// deviation of observed data.
+  double genericMetric(std::vector<double>& observed,
+    std::vector<double>& predicted, enum MetricType mt, enum DifferenceType dt);
   virtual ~SurfpackModel();
   virtual void scaler(ModelScaler* ms);
   ModelScaler* scaler() const;
