b0ee445d33276cef95a0c43d8bf6a24c358ccc2e
wjbohnh <wjbohnh@5a19d2f1-2ada-482b-9066-51d89674df4a>
2011-04-06 15:19:14 +0000

    Fixed F77_FUNC related compile errors by reverting changes I made yesterday (Rev 578) to the three key header files that involve F77_FUNC definition/use.
    Should fix the autotools builds for the development team, although the CMake
    build WILL FAIL (I'll hopefully get the CMake build restored later today).
    
    
    
    git-svn-id: https://software.sandia.gov/svn/surfpack/trunk@579 5a19d2f1-2ada-482b-9066-51d89674df4a

--- src/surfpack.h
+++ src/surfpack.h
@@ -11,9 +11,6 @@
 
 #ifdef HAVE_CONFIG_H
 #include "surfpack_config.h"
-#else
-// Use the CMake generated fortran name mangling macros (eliminate warnings)
-//wjbPOUND nowInSysHdrs -- include "surf77_config.h"
 #endif
 #include "surfpack_system_headers.h"
 
@@ -39,6 +36,68 @@ enum MetricType {
   MT_ROOT_MEAN
 };
 
+// Perform LU factorization
+#define DGETRF_F77 F77_FUNC(dgetrf,DGETRF)
+#ifdef __cplusplus
+extern "C"  /* prevent C++ name mangling */
+#endif
+void DGETRF_F77(const int* m, const int* n, double* a, const int* lda,
+		int* ipiv, int* info);
+
+// Compute the inverse of a matrix expressed as an LU decomposition
+// (i.e., call dgetrf on the matrix first)
+#define DGETRI_F77 F77_FUNC(dgetri,DGETRI)
+#ifdef __cplusplus
+extern "C"  /* prevent C++ name mangling */
+#endif
+void DGETRI_F77(const int* n, double* a, const int* lda, const int* ipiv,
+		double* work, const int* lwork, int* info);
+
+// Matrix-vector multiplication
+#define DGEMV_F77 F77_FUNC(dgemv,DGEMV)
+#ifdef __cplusplus
+extern "C"  /* prevent C++ name mangling */
+#endif
+void DGEMV_F77(char* trans, const int* m, const int* n, const double* alpha, 
+	       const double* A, const int* lda, const double* x,
+	       const int* incx, const double* beta, double* y, const int* incy);
+
+// Matrix-matrix multiplication
+#define DGEMM_F77 F77_FUNC(dgemm,DGEMM)
+#ifdef __cplusplus
+extern "C"  /* prevent C++ name mangling */
+#endif
+void DGEMM_F77(char* transa, char* transb, const int* m, const int* n,
+	       const int* k, const double* alpha, const double* A,
+	       const int* lda, const double* B, const int* ldb, 
+	       const double* beta, double* C, const int* ldc);
+
+// Vector-vector inner product
+#define DDOT_F77 F77_FUNC(ddot, DDOT)
+#ifdef __cplusplus
+extern "C" /* prevents C++ name mangling */
+#endif
+double DDOT_F77(const int* n, const double* x, const int* incx,
+		const double* y, const int* incy);
+
+// Least-squares solution to linear system of equations
+#define DGELS_F77 F77_FUNC(dgels,DGELS)
+#ifdef __cplusplus
+extern "C" /* prevent C++ name mangling */
+#endif
+void DGELS_F77(const char* trans, const int* nrows, const int* ncols,
+	       const int* nrhs, double* A, const int* lda, double* b,
+	       const int* ldb, double* work, const int* lwork, int* info);
+
+// Performs least-squares solve subject to equality constraints
+#define DGGLSE_F77 F77_FUNC(dgglse,DGGLSE)
+#ifdef __cplusplus
+extern "C" /* prevent C++ name mangling */
+#endif
+void DGGLSE_F77(const int* m, const int* n, const int* p, double* A,
+		const int* lda, double* B, const int* ldb, double* c,
+		double* d, double* x, double* work, const int* lwork,
+		int* info);
 
 namespace surfpack {
 
--- src/surfpack_system_headers.h
+++ src/surfpack_system_headers.h
@@ -8,13 +8,6 @@
 
 #ifndef SURFPACK_SYSTEM_HEADERS_H
 #define SURFPACK_SYSTEM_HEADERS_H
-
-#ifdef HAVE_CONFIG_H
-#include "surfpack_config.h"
-#else
-#include "surf77_config.h"
-#endif
-
 #include <algorithm>
 #include <limits>
 #include <cassert>
@@ -43,6 +36,7 @@
 #include <string>
 #include <sys/time.h>
 #include <vector>
+#endif
 
 typedef std::vector<double> VecDbl;
 typedef std::vector<double>::const_iterator VecDblIt;
@@ -56,71 +50,3 @@ typedef std::set< unsigned > SetUns;
 typedef std::pair< std::string, std::string > ModelParam;
 typedef std::map< std::string, std::string> ParamMap;
 typedef std::pair< std::string, ParamMap> Command;
-
-
-// Commonly used BLAS/LAPACK subroutines
-
-#ifdef HAVE_CONFIG_H
-// Tolerate F77_FUNC macro redefinition warnings in the autotools build
-#define DGETRF_F77 F77_FUNC(dgetrf,DGETRF)
-#define DGETRI_F77 F77_FUNC(dgetri,DGETRI)
-#define DGEMV_F77  F77_FUNC(dgemv,DGEMV)
-#define DGEMM_F77  F77_FUNC(dgemm,DGEMM)
-#define DDOT_F77   F77_FUNC(ddot, DDOT)
-#define DGELS_F77  F77_FUNC(dgels,DGELS)
-#define DGGLSE_F77 F77_FUNC(dgglse,DGGLSE)
-
-#else
-// Use the CMake generated fortran name mangling macros (eliminate warnings)
-#define DGETRF_F77 SURF77_GLOBAL(dgetrf,DGETRF)
-#define DGETRI_F77 SURF77_GLOBAL(dgetri,DGETRI)
-#define DGEMV_F77  SURF77_GLOBAL(dgemv,DGEMV)
-#define DGEMM_F77  SURF77_GLOBAL(dgemm,DGEMM)
-#define DDOT_F77   SURF77_GLOBAL(ddot, DDOT)
-#define DGELS_F77  SURF77_GLOBAL(dgels,DGELS)
-#define DGGLSE_F77 SURF77_GLOBAL(dgglse,DGGLSE)
-#endif
-
-#ifdef __cplusplus
-extern "C" { // prevent C++ name mangling
-#endif
-
-// Perform LU factorization
-void DGETRF_F77(const int* m, const int* n, double* a, const int* lda,
-                int* ipiv, int* info);
-
-// Compute the inverse of a matrix expressed as an LU decomposition
-// (i.e., call dgetrf on the matrix first)
-void DGETRI_F77(const int* n, double* a, const int* lda, const int* ipiv,
-                double* work, const int* lwork, int* info);
-
-// Matrix-vector multiplication
-void DGEMV_F77(char* trans, const int* m, const int* n, const double* alpha,
-               const double* A, const int* lda, const double* x,
-               const int* incx, const double* beta, double* y, const int* incy);
-
-// Matrix-matrix multiplication
-void DGEMM_F77(char* transa, char* transb, const int* m, const int* n,
-               const int* k, const double* alpha, const double* A,
-               const int* lda, const double* B, const int* ldb,
-               const double* beta, double* C, const int* ldc);
-
-// Vector-vector inner product
-double DDOT_F77(const int* n, const double* x, const int* incx,
-                const double* y, const int* incy);
-
-// Least-squares solution to linear system of equations
-void DGELS_F77(const char* trans, const int* nrows, const int* ncols,
-               const int* nrhs, double* A, const int* lda, double* b,
-               const int* ldb, double* work, const int* lwork, int* info);
-
-// Performs least-squares solve subject to equality constraints
-void DGGLSE_F77(const int* m, const int* n, const int* p, double* A,
-                const int* lda, double* B, const int* ldb, double* c,
-                double* d, double* x, double* work, const int* lwork,
-                int* info);
-
-}  // end extern "C" of BLAS/LAPACK subroutines
-
-#endif // SURFPACK_SYSTEM_HEADERS_H
-
